401d6c5de277f661ff09e2c7436900af6332c2aa
Your Name <you@example.com>
2022-08-31 17:00:44 +0900

    updateboard

---
+++ src/main/java/com/cloud/security/CustomUserDetailsServie.java
@@ -0,0 +1,31 @@
+package com.cloud.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.cloud.domain.MemberVO;
+import com.cloud.mapper.MemberMapper;
+import com.cloud.security.domain.CustomUser;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class CustomUserDetailsServie implements UserDetailsService {
+	
+	@Autowired
+	private MemberMapper mapper;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		log.warn("Load User By UserName : " + username);
+		
+		MemberVO vo = mapper.read(username);
+		
+		log.warn("queried by member mapper : " + vo);
+		
+		return vo == null? null : new CustomUser(vo);
+	}
+
+}
